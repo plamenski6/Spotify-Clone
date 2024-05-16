f89ffea9d9fd3ae6e499aa11854b6b972c8491f5
Plamen Shumanski <plamen1030@abv.bg>
2024-05-16 11:34:56 +0300

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<input />
+<select>
+	<option>Option</option>
+</select>
+<button>Button</button>
+
+<style lang="scss">
+	h1 {
+		color: red;
+		font-size: functions.toRem(36);
+	}
+</style>
